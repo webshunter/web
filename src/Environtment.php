53619bbf337dd51868475b7dfd2ec3ff603bb1be
Gugus Darmayanto <78706454+webshunter@users.noreply.github.com>
2024-02-17 09:58:55 +0700

    Update Environtment.php

--- src/Environtment.php
+++ src/Environtment.php
@@ -1,1 +1,25 @@
+<?php
+namespace Gugusd999\Web;
 
+class Environtment{
+   public function __construct($path = "./.env"){
+       if(file_exists($path)){
+          $getenv = parse_ini_file($path);
+          foreach ($getenv as $key => $value) {
+              $this->setEnv($key, $value);
+          }
+        }
+   }
+    
+  public function setEnv($name="", $val = ""){
+      if(!defined($name)){
+          $_ENV[$name] = $val;
+          define($name, $val);
+      }
+  }
+
+  public static function get($name=""){
+      return $_ENV[$name]?$_ENV[$name]:null;
+  }
+  
+}
