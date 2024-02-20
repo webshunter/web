796fdafd13a5b597eaea3ac1ec257e8e9f1e1c67
Gugus Darmayanto <78706454+webshunter@users.noreply.github.com>
2024-02-20 19:34:35 +0700

    Update Files.php

--- src/Files.php
+++ src/Files.php
@@ -85,5 +85,54 @@ class Files
         fclose($myfile);
         return $er;
     }
+    
+    function dir($directory = "./") {
+        // Get the list of files and folders in the directory
+        $contents = scandir($directory);
+    
+        // Exclude "." and ".." from the list
+        $contents = array_diff($contents, array('.', '..'));
+    
+        // Initialize the array to hold the result
+        $result = [];
+    
+        // Iterate through each item
+        foreach ($contents as $item) {
+            $itemPath = $directory . '/' . $item;
+    
+            // Determine if it's a folder or file
+            if (is_dir($itemPath)) {
+                // If it's a folder, add it to the result with type "folder"
+                $result[] = [
+                    'name' => $item,
+                    'type' => 'folder'
+                ];
+            } else {
+                // If it's a file, determine its extension and add it to the result with the corresponding type
+                $extension = pathinfo($itemPath, PATHINFO_EXTENSION);
+                $result[] = [
+                    'name' => $item,
+                    'type' => $extension ? $extension . ' file' : 'file'
+                ];
+            }
+        }
+    
+        // Custom comparison function to sort folders first, then files, both alphabetically by name
+        usort($result, function($a, $b) {
+            if ($a['type'] === 'folder' && $b['type'] === 'folder') {
+                return strcmp($a['name'], $b['name']);
+            } elseif ($a['type'] === 'folder') {
+                return -1;
+            } elseif ($b['type'] === 'folder') {
+                return 1;
+            } else {
+                return strcmp($a['name'], $b['name']);
+            }
+        });
+    
+        // Return the sorted list
+        return $result;
+
+    }
 
 }
